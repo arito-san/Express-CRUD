a3b261c8f0d33e10d09dea9b323279df25bc3bd7
Ari <88846563+arito-san@users.noreply.github.com>
2023-02-22 22:16:52 -0300

    update and delete user, this is the last commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { createUser, searchAllUser, searchUser } = require("../controllers/index.js");
+const { createUser, searchAllUser, searchUser, editUser, deletetUser } = require("../controllers/index.js");
 const router = express.Router();
 
 //rotas
 router.post('/createAccount', createUser);
 router.get('/searchAllUser', searchAllUser);
 router.post('/searchUser', searchUser);
+router.put('/editUser', editUser);
+router.post('/deleteUser', deletetUser);
 
 
 module.exports = router
